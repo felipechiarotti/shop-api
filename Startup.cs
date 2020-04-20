5add17e7f503ec59883e18e4bde8818220f17572
felipechi97@gmail.com <213616qpo>
2020-04-20 16:00:26 -0300

    Corrigido inicializador de banco

--- Startup.cs
+++ Startup.cs
@@ -55,10 +55,10 @@ namespace Shop
 
             });
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-            //services.AddDbContext<DataContext>(
-            //    opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString"))
-            //);
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(
+                opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString"))
+            );
 
 
             services.AddSwaggerGen(c =>
